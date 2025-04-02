cac782c0f55dbd648f14d54e64a992cd113389fd
Ubu4210 <jhcfree01@gmail.com>
2025-04-02 17:21:26 +0900

    rule 2_파이프라인

--- auto_pipeline.py
+++ auto_pipeline.py
@@ -3,7 +3,7 @@ from crawler import update_one_year_news
 from sentiment import (
     fetch_news_from_mysql, # 뉴스 불러오기
     analyze_sentiment, # 감성 분석
-    update_sentiment_in_mysql, # 감성 점수 news 테이블에 저장
+    update_sentiment_in_mysql, # 감성 점수를 news 테이블에 저장
     calculate_daily_avg_sentiment, # 일별 평균 감성 점수 계산
     save_daily_avg_to_mysql # 일별 평균 감성 점수를 avg_sentiment 테이블에 저장         
 )
@@ -11,6 +11,21 @@ from top_stock_price import run_top_stock_price
 import pymysql
 from config import DB_CONFIG
 
+""" 데이터베이스 생성 """
+
+def create_database_if_not_exists():
+    db_config_no_db = DB_CONFIG.copy()
+    db_config_no_db.pop('database', None)
+
+    conn = pymysql.connect(**db_config_no_db)
+    cursor = conn.cursor()
+    # 기본 문자 인코딩, 정렬 및 문자열 비교 설정
+    cursor.execute("CREATE DATABASE IF NOT EXISTS news_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print("데이터베이스 확인 및 생성 완료")
+    
 """ 테이블 생성 """
 
 def create_tables_if_not_exist():
@@ -57,32 +72,36 @@ def create_tables_if_not_exist():
 """ 파이프라인 """
 
 def run_auto_pipeline():
-    print(f"\n {datetime.now().strftime('%H:%M:%S')} | 테이블 체크 및 뉴스 수집 시작")
-    # 1. 테이블 생성
+    print(f"\n데이터베이스 체크 및 생성")
+    # 1. 데이터베이스 생성
+    create_database_if_not_exists()
+    
+    print(f"\n테이블 체크 및 뉴스 수집 시작")
+    # 2. 테이블 생성
     create_tables_if_not_exist()
     
-    # 2. 뉴스 수집
+    # 3. 뉴스 수집
     update_one_year_news()
 
     print("감성 분석 시작")
-    # 3. MySQL에서 뉴스 데이터 가져오기
+    # 4. 뉴스 데이터 가져오기
     news_data = fetch_news_from_mysql()
     if news_data:
-        # 4. 감성 분석 수행
+        # 5. 감성 분석 수행
         analyzed_data = [(analyze_sentiment(title), news_id) for news_id, title in news_data]
-        # 5. 감성 점수 업데이트
+        # 6. 감성 점수 업데이트
         update_sentiment_in_mysql(analyzed_data)
     else:
         print("감성 분석할 뉴스가 없습니다.")
 
     print("일별 평균 감성 점수 계산 및 저장")
-    # 6. 일별 평균 감성 점수 계산 및 저장
+    # 7. 일별 평균 감성 점수 계산 
     daily_averages = calculate_daily_avg_sentiment()
-    # 7. 일별 평균 감성 점수 저장
+    # 8. 일별 평균 감성 점수 저장
     save_daily_avg_to_mysql(daily_averages)
     
     print("주식 데이터 수집 시작")  
-    # 8. 주식 데이터 수집 및 저장
+    # 9. 주식 데이터 수집 및 저장
     run_top_stock_price()  
     
     print("파이프라인 완료")
