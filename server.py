35aedbb7376210846c4b79a6c8b36404105b41f2
Hyomin Kang <lala1225@seoultech.ac.kr>
2025-05-16 03:23:18 +0900

    작업 중인 변경사항 커밋

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, jsonify, request, send_from_directory, redirect
 import os
 import pymysql
 import config
+import pandas as pd
 from datetime import datetime
 from auto_pipeline import run_auto_pipeline
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -20,7 +21,66 @@ rules = [
 def ping():
     return '서버는 살아있습니다.'
 
+@app.route('/backtest')
+def backtest():
+    timestamp = int(datetime.now().timestamp())
 
+    # 경로 기준을 fundweb 상위 디렉토리의 rule_2_결과로 설정
+    base_dir = os.path.abspath(os.path.join(app.root_path, '..', 'rule_2_결과'))
+
+    # 성능 지표
+    score_table = None
+    score_path = os.path.join(base_dir, "성능 지표.csv")
+    if os.path.exists(score_path):
+        df_score = pd.read_csv(score_path)
+        df_score = df_score.sort_values(by="f1_score", ascending=False).head(10)  # 상위 10개만
+        score_table = df_score.to_dict(orient="records")
+
+    # 매매 로그
+    trade_log_table = None
+    trade_log_path = os.path.join(base_dir, "매매 로그.csv")
+    if os.path.exists(trade_log_path):
+        df_log = pd.read_csv(trade_log_path)
+        df_log = df_log.sort_values("buy_date", ascending=False).head(10)
+        trade_log_table = df_log.to_dict(orient="records")
+
+    # 거래 성공률
+    success_rate_table = None
+    success_rate_path = os.path.join(base_dir, "거래 성공률.csv")
+    if os.path.exists(success_rate_path):
+        df_success = pd.read_csv(success_rate_path)
+        df_success = df_success.sort_values(by="success_rate", ascending=False).head(10)
+        success_rate_table = df_success.to_dict(orient="records")
+
+    return render_template(
+        'backtest.html',
+        timestamp=timestamp,
+        score_table=score_table,
+        trade_log_table=trade_log_table,
+        success_rate_table=success_rate_table
+    )
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route('/run-backtest', methods=['POST'])
+def run_backtest():
+    try:
+        run_auto_pipeline()
+
+        return jsonify({"status": "ok"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+    
+@app.route('/external/<path:filename>')
+def external_static(filename):
+    external_dir = os.path.abspath(os.path.join(app.root_path, '..', 'rule_2_결과'))
+    return send_from_directory(external_dir, filename)
+
+@app.route("/")
+def root():
+    return redirect("/home")
 
 # 🔹 Flask 실행
 if __name__ == "__main__":
