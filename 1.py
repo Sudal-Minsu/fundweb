79990aaaf8617e1f981baeb542b3217155b882a8
Minsu Ko <abcabc6763@naver.com>
2025-05-23 15:47:04 +0900

    1.py 수정

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
 from functions import check_account, get_auth_info
 
 app_key, app_secret, access_token = get_auth_info()
-check_account(access_token, app_key, app_secret)
+#check_account(access_token, app_key, app_secret)
